e932732c41ea695125d13b8c69ea452a0db4276c
DJnoxxys <56643215+DJnoxxys@users.noreply.github.com>
2020-04-08 16:39:14 +0200

    img maj

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/idb.js');
 importScripts('js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v6';
-var CACHE_DYNAMIC_NAME = 'dynamic-v6';
+var CACHE_STATIC_NAME = 'static-v3';
+var CACHE_DYNAMIC_NAME = 'dynamic-v3';
 var STATIC_FILES=[
     //pages
     'index.html',
